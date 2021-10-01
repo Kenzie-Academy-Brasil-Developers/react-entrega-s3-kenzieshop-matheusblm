1d5a9664c580a4284ce550250429da4b3ad343f6
hank <matheus_bueno10@hotmail.com>
2021-10-01 13:29:09 -0300

    fix: arrumado formatacao dos precoes e preco final

--- src/Components/Cart/index.js
+++ src/Components/Cart/index.js
@@ -1,18 +1,19 @@
 import { useSelector } from "react-redux";
 import CardProduct from "../CardProduct";
-import { Grid, Typography, Card } from "@material-ui/core";
+import { Grid, Typography, Box } from "@material-ui/core";
 function Cart() {
   const products = useSelector((store) => store.cart);
 
   return (
     <>
-      <Card>
+      <Box sx={{ display: "flex", flexDirection: "row-reverse" }}>
         <Grid
           container
           flexDirection={"column"}
           spacing={1}
           marginTop="80px"
           ml={0}
+          flexShrink="6"
         >
           <Typography variant="h5" mr={2} alignSelf="center">
             Preco Total:{" "}
@@ -22,18 +23,17 @@ function Cart() {
               }, 0)
               .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
           </Typography>
-          <Typography variant="h5" mr={2} alignSelf="center">
-            Seu carrinho de compra:
-          </Typography>
+          <Typography variant="h5" mr={2} alignSelf="center"></Typography>
         </Grid>
-      </Card>
-      <Grid container justify="center" spacing={4} marginTop="70px">
-        {products.map((product) => (
-          <Grid item xs={12} sm={6} md={4} lg={3}>
-            <CardProduct key={product.id} product={product} remove />
-          </Grid>
-        ))}
-      </Grid>
+
+        <Grid container justify="center" spacing={4} marginTop="70px">
+          {products.map((product) => (
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <CardProduct key={product.id} product={product} remove />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </>
   );
 }
